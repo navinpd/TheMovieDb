74f96ad5a963d6e2c09e2c28a3be8db736427602
Navin prasad <navin.pd@gmail.com>
2022-03-31 10:31:28 +0800

    initial commit

---
+++ app/src/main/java/com/api/moviedb/ui/now_playing/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.api.moviedb.ui.now_playing
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
